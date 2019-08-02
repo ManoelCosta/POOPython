9994b2fb635e25423ae457d934c12680e6a91489
Manoel Costa <ManoelCosta>
2019-08-02 17:26:04 -0300

    Programa04: Programa é um jogo de adivinhações. Um valor é atribuido dentro da variável numero_secreto, o usuário pode tentar adivinhar o numero o valor será guardado na variável chute. A variavel acertou, maior e menor ficaram com a comparação igual(==), maior(>) e menor(<) entre as variaveis chute e numero_secreto. a condição if-elif é criada para mostrar o desempenho do usuário.

---
+++ programa04.py
@@ -0,0 +1,14 @@
+numero_secreto = 32
+chute = int(input('Tente adivinhar o numero:\n'))
+
+acertou = chute == numero_secreto
+maior = chute > numero_secreto
+menor = chute < numero_secreto
+
+if acertou:
+    print('Parabéns você acertou')
+elif maior:
+    print('Que pena, você errou, chutou um numero maior')
+elif menor:
+    print('Que pena, você errou, chutou um numero menor')
+print('Fim de jogo')
