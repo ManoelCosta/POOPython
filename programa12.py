5fcc596aa8e790cb0e154288c9361be5795bb93f
Manoel Costa <ManoelCosta>
2019-08-05 19:03:48 -0300

    mudança do nome do dicionario de dados_do_usuario para pessoa

--- programa12.py
+++ programa12.py
@@ -1,11 +1,11 @@
 # programa le dados do usuario, armazena num dict() e imprime os valores na tela
 
-dados_do_usuario = {'Nome': '', 'Idade': '', 'Cidade': ''}
+pessoa = {'Nome': '', 'Idade': '', 'Cidade': ''}
 
-dados_do_usuario['Nome'] = input('Informe o seu nome:\n')
-dados_do_usuario['Idade'] = int(input('Informe a idade:\n'))
-dados_do_usuario['Cidade'] = input('Informe sua cidade:\n')
+pessoa['Nome'] = input('Informe o seu nome:\n')
+pessoa['Idade'] = int(input('Informe a idade:\n'))
+pessoa['Cidade'] = input('Informe sua cidade:\n')
 
-print('Nome:', dados_do_usuario['Nome'])
-print('Idade:', dados_do_usuario['Idade'])
-print('Cidade:', dados_do_usuario['Cidade'])
+print('Nome:', pessoa['Nome'])
+print('Idade:', pessoa['Idade'])
+print('Cidade:', pessoa['Cidade'])
