f49d0398bb956461b2e64e8b623d5acceecaab3d
ManoelCosta <47435630+ManoelCosta@users.noreply.github.com>
2019-08-07 17:27:40 -0300

    Update programa11.py
    inclusão da linha 9 que mostra as notas ne tela

--- programa11.py
+++ programa11.py
@@ -6,6 +6,7 @@ for a in range(1, 5):
     nota = float(input('Informe a {}ª nota:\n'.format(a)))
     notas.append(nota)
 for b in range(0, len(notas)):
+    print('{}° nota: {:.1f}pts'.format((b + 1), notas[b]))
     soma += notas[b]
 media = soma/len(notas)
 print('A média vale: {:.2f}'.format(media))
